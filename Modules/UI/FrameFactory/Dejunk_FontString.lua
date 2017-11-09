34ba6a56bcc09fedf05248bf11388467a2b8a8a9
Dethanyel <Dethanyel@gmail.com>
2017-11-09 17:57:07 -0500

    LICENSE.md

--- Modules/UI/FrameFactory/Dejunk_FontString.lua
+++ Modules/UI/FrameFactory/Dejunk_FontString.lua
@@ -1,22 +1,4 @@
---[[
-Copyright 2017 Justin Moody
-
-Dejunk is distributed under the terms of the GNU General Public License.
-You can redistribute it and/or modify it under the terms of the license as
-published by the Free Software Foundation.
-
-This addon is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this addon. If not, see <http://www.gnu.org/licenses/>.
-
-This file is part of Dejunk.
---]]
-
--- Dejunk_FontString: contains FrameFactory functions to create and release a font string tailored to Dejunk.
+-- Dejunk_FontString: contains FrameFactory functions to create a font string tailored to Dejunk.
 
 local AddonName, DJ = ...
 
