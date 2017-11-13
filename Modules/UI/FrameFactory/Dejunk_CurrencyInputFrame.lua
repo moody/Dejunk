97db0013ad181f515a89612c359c0173024ce348
Dethanyel <Dethanyel@gmail.com>
2017-11-13 16:49:35 -0500

    Added CurrencyInputFrame

---
+++ Modules/UI/FrameFactory/Dejunk_CurrencyInputFrame.lua
@@ -0,0 +1,141 @@
+-- Dejunk_CurrencyInputFrame: contains FrameFactory functions to create a CheckButton & EditBox combo frame.
+
+local AddonName, DJ = ...
+
+-- Libs
+local L = LibStub('AceLocale-3.0'):GetLocale(AddonName)
+
+-- Upvalues
+local abs, tonumber = abs, tonumber
+
+-- Dejunk
+local FrameFactory = DJ.FrameFactory
+
+local DejunkDB = DJ.DejunkDB
+local Tools = DJ.Tools
+local FrameCreator = DJ.FrameCreator
+
+--[[
+//*******************************************************************
+//                  CurrencyInputFrame Functions
+//*******************************************************************
+--]]
+
+local moneyIconsTexture = "Interface\\MoneyFrame\\UI-MoneyIcons"
+
+function FrameFactory:CreateCurrencyInputFrame(parent, font, svKey)
+  assert(svKey and type(DejunkDB.SV[svKey]) == "table")
+
+  local frame = FrameCreator:CreateFrame(parent)
+  frame.FF_ObjectType = "CurrencyInputFrame"
+
+  -- Gold
+  local goldEditBoxFrame = self:CreateEditBoxFrame(frame, font, 4)
+  frame.GoldEditBoxFrame = goldEditBoxFrame
+
+  local goldTexture = self:CreateTexture(frame)
+  goldTexture:SetTexture(moneyIconsTexture)
+  goldTexture:SetTexCoord(0, 0.25, 0, 1)
+  frame.GoldTexture = goldTexture
+
+  -- Silver
+  local silverEditBoxFrame = self:CreateEditBoxFrame(frame, font, 2)
+  frame.SilverEditBoxFrame = silverEditBoxFrame
+
+  local silverTexture = self:CreateTexture(frame)
+  silverTexture:SetTexture(moneyIconsTexture)
+  silverTexture:SetTexCoord(0.25, 0.5, 0, 1)
+  frame.SilverTexture = silverTexture
+
+  -- Copper
+  local copperEditBoxFrame = self:CreateEditBoxFrame(frame, font, 2)
+  frame.CopperEditBoxFrame = copperEditBoxFrame
+
+  local copperTexture = self:CreateTexture(frame)
+  copperTexture:SetTexture(moneyIconsTexture)
+  copperTexture:SetTexCoord(0.5, 0.75, 0, 1)
+  frame.CopperTexture = copperTexture
+
+  -- Initialize points
+  goldEditBoxFrame:SetPoint("TOPLEFT", frame)
+  goldTexture:SetPoint("TOPLEFT", goldEditBoxFrame, "TOPRIGHT", 0, 0)
+
+  silverEditBoxFrame:SetPoint("TOPLEFT", goldTexture, "TOPRIGHT", 0, 0)
+  silverTexture:SetPoint("TOPLEFT", silverEditBoxFrame, "TOPRIGHT", 0, 0)
+
+  copperEditBoxFrame:SetPoint("TOPLEFT", silverTexture, "TOPRIGHT", 0, 0)
+  copperTexture:SetPoint("TOPLEFT", copperEditBoxFrame, "TOPRIGHT", 0, 0)
+
+  -- Gold accept
+  goldEditBoxFrame.EditBox:SetScript("OnEditFocusLost", function(self)
+    local value = tonumber(self:GetText())
+    if value then DejunkDB.SV[svKey].Gold = floor(abs(value)) end
+    self:SetText(DejunkDB.SV[svKey].Gold)
+  end)
+
+  -- Silver accept
+  silverEditBoxFrame.EditBox:SetScript("OnEditFocusLost", function(self)
+    local value = tonumber(self:GetText())
+    if value then DejunkDB.SV[svKey].Silver = floor(abs(value)) end
+    self:SetText(DejunkDB.SV[svKey].Silver)
+  end)
+
+  -- Copper accept
+  copperEditBoxFrame.EditBox:SetScript("OnEditFocusLost", function(self)
+    local value = tonumber(self:GetText())
+    if value then DejunkDB.SV[svKey].Copper = floor(abs(value)) end
+    self:SetText(DejunkDB.SV[svKey].Copper)
+  end)
+
+  -- Gets the minimum width of the frame.
+  function frame:GetMinWidth()
+    return 150
+  end
+
+  -- Gets the minimum height of the frame.
+  function frame:GetMinHeight()
+    return goldEditBoxFrame:GetHeight()
+  end
+
+  -- Resizes the frame.
+  function frame:Resize()
+    goldEditBoxFrame:Resize()
+    silverEditBoxFrame:Resize()
+    copperEditBoxFrame:Resize()
+
+    for _, tex in pairs({goldTexture, silverTexture, copperTexture}) do
+      tex:SetWidth(self:GetMinHeight())
+      tex:SetHeight(self:GetMinHeight())
+    end
+
+    self:SetWidth(self:GetMinWidth())
+    self:SetHeight(self:GetMinHeight())
+  end
+
+  function frame:Refresh()
+    goldEditBoxFrame:Refresh()
+    silverEditBoxFrame:Refresh()
+    copperEditBoxFrame:Refresh()
+
+    -- SV
+    goldEditBoxFrame.EditBox:SetText(DejunkDB.SV[svKey].Gold)
+    silverEditBoxFrame.EditBox:SetText(DejunkDB.SV[svKey].Silver)
+    copperEditBoxFrame.EditBox:SetText(DejunkDB.SV[svKey].Copper)
+  end
+
+  frame:Refresh()
+
+  return frame
+end
+
+function FrameFactory:EnableCurrencyInputFrame(frame)
+  frame.GoldEditBoxFrame.EditBox:SetEnabled(true)
+  frame.SilverEditBoxFrame.EditBox:SetEnabled(true)
+  frame.CopperEditBoxFrame.EditBox:SetEnabled(true)
+end
+
+function FrameFactory:DisableCurrencyInputFrame(frame)
+  frame.GoldEditBoxFrame.EditBox:SetEnabled(false)
+  frame.SilverEditBoxFrame.EditBox:SetEnabled(false)
+  frame.CopperEditBoxFrame.EditBox:SetEnabled(false)
+end
