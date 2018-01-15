e089769d5b70307a81d6b1f9060bc26d1baacd91
Dethanyel <Dethanyel@gmail.com>
2018-01-15 07:40:43 -0500

    added zhTW locale

---
+++ Locales/zhTW.lua
@@ -0,0 +1,29 @@
+-- Dejunk: zhTW (Traditional Chinese) localization file.
+-- Translation by BNSSNB (https://wow.curseforge.com/members/BNSSNB).
+
+local AddonName, AddonTable = ...
+local L = LibStub('AceLocale-3.0'):NewLocale(AddonName, 'zhTW')
+if not L then return end
+
+L["ADDED_ITEM_TO_LIST"] = "新增%s到%s。"
+L["AUTO_DESTROY_TEXT"] = "自動銷毀"
+L["AUTO_DESTROY_TOOLTIP"] = "當此事窗關閉時定期銷毀垃圾物品"
+L["AUTO_REPAIR_TEXT"] = "自動修理"
+L["AUTO_REPAIR_TOOLTIP"] = "當開啟商店視窗介面時自動修理物品。"
+L["AUTO_SELL_TEXT"] = "自動賣出"
+L["AUTO_SELL_TOOLTIP"] = "當開啟商店視窗介面時自動賣出垃圾物品。"
+L["BACK_TEXT"] = "返回"
+L["BINDINGS_ADD_TO_LIST_TEXT"] = "新增到%s"
+L["BINDINGS_REMOVE_FROM_LIST_TEXT"] = "從%s移除"
+L["BINDINGS_TOGGLE_OPTIONS_TEXT"] = "切換選項"
+L["BY_CATEGORY_TEXT"] = "依據類別"
+L["BY_QUALITY_TEXT"] = "依據品質"
+L["BY_TYPE_TEXT"] = "依據類型"
+L["CANNOT_DEJUNK_WHILE_DESTROYING"] = "無法消除垃圾當物品已被銷毀。"
+L["CANNOT_DEJUNK_WHILE_LISTS_UPDATING"] = "無法消除垃圾當%s與%s已經更新。"
+L["CANNOT_DESTROY_WHILE_DEJUNKING"] = "無法銷毀當物品已經被清除。"
+L["CANNOT_DESTROY_WHILE_LIST_UPDATING"] = "無法銷毀當%s已經更新。"
+L["CHARACTER_SPECIFIC_TEXT"] = "角色專屬設定"
+L["CHARACTER_SPECIFIC_TOOLTIP"] = "點擊於共用設定與角色專屬設定中切換。"
+L["COLOR_SCHEME_SET_TEXT"] = "著色方案設定為%s。"
+L["COLOR_SCHEME_TEXT"] = "方案"
