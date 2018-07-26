abd871fc8a87a894f15c017ad67524bcc5dee437
moody <dethanyel@gmail.com>
2018-07-25 20:19:12 -0400

    updated to reduce duplicate code and for changes to DFL.EditBox

--- Modules/UI/Objects/CurrencyField.lua
+++ Modules/UI/Objects/CurrencyField.lua
@@ -7,21 +7,67 @@ local DFL = Addon.Libs.DFL
 
 -- Addon
 local CurrencyField = Addon.Objects.CurrencyField
-CurrencyField.Scripts = {}
 
 local Colors = Addon.Colors
 local DejunkDB = Addon.DejunkDB
 
 -- Consts
 local MONEY_ICONS_TEXTURE = "Interface\\MoneyFrame\\UI-MoneyIcons"
-local TEXTURE_SIZE = 12
 local GOLD_TEX_COORD = {0, 0.25, 0, 1}
 local SILVER_TEX_COORD = {0.25, 0.5, 0, 1}
 local COPPER_TEX_COORD = {0.5, 0.75, 0, 1}
 
-local MAX_GOLD_DIGIT = 2
+local MAX_GOLD_DIGIT = 3
 local MAX_SILVER_DIGIT = 2
 local MAX_COPPER_DIGIT = 2
+
+-- ============================================================================
+-- Local Functions
+-- ============================================================================
+
+local sizer = UIParent:CreateFontString(AddonName.."GetStringWidthSizer", "BACKGROUND")
+
+-- Approximates and returns the minimum width required for a font string.
+local function getStringWidth(font, numCharacters, numeric)
+  -- Make string of characters
+  local char = numeric and "9" or "W"
+  local text = ""
+  for i=1, numCharacters+2 do text = text..char end
+  -- Set font and text
+  sizer:SetFontObject(font)
+  sizer:SetText(text)
+  -- Return width
+  return sizer:GetStringWidth()
+end
+
+local function getUserValue(self)
+  return DejunkDB:Get(self._svKey)
+end
+
+local function setUserValue(self, value)
+  value = floor(abs(value))
+  DejunkDB:Set(self._svKey, value)
+end
+
+local function getEditBox(svKey, maxLetters, font)
+  local editBox = DFL.EditBox:Create(frame, maxLetters, true, font)
+  editBox:SetMinWidth(getStringWidth(font, maxLetters, true) + DFL:Padding(0.5))
+  editBox._editBox:SetScript("OnEditFocusGained", editBox._editBox.HighlightText)
+  editBox._editBox:SetJustifyH("CENTER")
+  editBox._svKey = svKey
+  editBox.GetUserValue = getUserValue
+  editBox.SetUserValue = setUserValue
+  return editBox
+end
+
+local function getTexture(parent, texCoord, size)
+  local texture = DFL.Creator:CreateTexture(parent)
+  texture:ClearAllPoints()
+  texture:SetTexture(MONEY_ICONS_TEXTURE)
+  texture:SetTexCoord(unpack(texCoord))
+  texture:SetSize(size, size)
+  return texture
+end
 
 -- ============================================================================
 -- Creation Function
@@ -30,68 +76,35 @@ local MAX_COPPER_DIGIT = 2
 function CurrencyField:Create(parent, svKey, font)
   assert((type(svKey) == "string") and (type(DejunkDB:Get(svKey)) == "table"))
 
-  local frame = DFL.Frame:Create(parent,
-    DFL.Alignments.LEFT, DFL.Directions.RIGHT)
+  local frame = DFL.Frame:Create(parent, DFL.Alignments.LEFT, DFL.Directions.RIGHT)
   frame:SetSpacing(DFL:Padding(0.5))
 
-  -- Gold
-  local goldEditBox = DFL.EditBox:Create(frame, MAX_GOLD_DIGIT, true, font)
-  goldEditBox._editBox:SetJustifyH("CENTER")
-  goldEditBox._editBox._svKey = svKey..".Gold"
-  goldEditBox:SetMinWidth(35)
+  -- Create edit boxes
+  local goldEditBox = getEditBox(svKey..".Gold", MAX_GOLD_DIGIT, font)
+  local silverEditBox = getEditBox(svKey..".Silver", MAX_SILVER_DIGIT, font)
+  local copperEditBox = getEditBox(svKey..".Copper", MAX_COPPER_DIGIT, font)
+
+  -- Set up tabbing
+  goldEditBox:SetPrevTabTarget(copperEditBox._editBox)
+  goldEditBox:SetNextTabTarget(silverEditBox._editBox)
+  silverEditBox:SetPrevTabTarget(goldEditBox._editBox)
+  silverEditBox:SetNextTabTarget(copperEditBox._editBox)
+  copperEditBox:SetPrevTabTarget(silverEditBox._editBox)
+  copperEditBox:SetNextTabTarget(goldEditBox._editBox)
+
+  -- Texture size = height of font
+  local _, textureSize = goldEditBox._editBox:GetFont()
+
+  -- Add to frame
   frame:Add(goldEditBox)
-
-  local goldTexture = DFL.Creator:CreateTexture(frame)
-  goldTexture:ClearAllPoints()
-  goldTexture:SetTexture(MONEY_ICONS_TEXTURE)
-  goldTexture:SetTexCoord(unpack(GOLD_TEX_COORD))
-  goldTexture:SetSize(TEXTURE_SIZE, TEXTURE_SIZE)
-  frame:Add(goldTexture)
-    
-  -- Silver
-  local silverEditBox = DFL.EditBox:Create(frame, MAX_SILVER_DIGIT, true, font)
-  silverEditBox._editBox:SetJustifyH("CENTER")
-  silverEditBox._editBox._svKey = svKey..".Silver"
-  silverEditBox:SetMinWidth(35)
+  frame:Add(getTexture(frame, GOLD_TEX_COORD, textureSize))
   frame:Add(silverEditBox)
-
-  local silverTexture = DFL.Creator:CreateTexture(frame)
-  silverTexture:ClearAllPoints()
-  silverTexture:SetTexture(MONEY_ICONS_TEXTURE)
-  silverTexture:SetTexCoord(unpack(SILVER_TEX_COORD))
-  silverTexture:SetSize(TEXTURE_SIZE, TEXTURE_SIZE)
-  frame:Add(silverTexture)
-
-  -- Copper
-  local copperEditBox = DFL.EditBox:Create(frame, MAX_COPPER_DIGIT, true, font)
-  copperEditBox._editBox:SetJustifyH("CENTER")
-  copperEditBox._editBox._svKey = svKey..".Copper"
-  copperEditBox:SetMinWidth(35)
+  frame:Add(getTexture(frame, SILVER_TEX_COORD, textureSize))
   frame:Add(copperEditBox)
-  
-  local copperTexture = DFL.Creator:CreateTexture(frame)
-  copperTexture:ClearAllPoints()
-  copperTexture:SetTexture(MONEY_ICONS_TEXTURE)
-  copperTexture:SetTexCoord(unpack(COPPER_TEX_COORD))
-  copperTexture:SetSize(TEXTURE_SIZE, TEXTURE_SIZE)
-  frame:Add(copperTexture)
-
-  -- References for OnTabPressed
-  -- ... Gold <-> Silver <-> Copper <-> Gold ...
-  goldEditBox._editBox._prevEditBox = copperEditBox._editBox
-  goldEditBox._editBox._nextEditBox = silverEditBox._editBox
-  
-  silverEditBox._editBox._prevEditBox = goldEditBox._editBox
-  silverEditBox._editBox._nextEditBox = copperEditBox._editBox
-
-  copperEditBox._editBox._prevEditBox = silverEditBox._editBox
-  copperEditBox._editBox._nextEditBox = goldEditBox._editBox
+  frame:Add(getTexture(frame, COPPER_TEX_COORD, textureSize))
 
   -- Mixins
   DFL:AddMixins(frame, self.Functions)
-  DFL:AddScripts(goldEditBox._editBox, self.Scripts)
-  DFL:AddScripts(silverEditBox._editBox, self.Scripts)
-  DFL:AddScripts(copperEditBox._editBox, self.Scripts)
 
   frame:SetColors()
   frame:Refresh()
@@ -110,53 +123,12 @@ do
     -- Refresh value of edit boxes
     for k, editBox in pairs(self:GetChildren()) do
       if DFL:IsType(editBox, DFL.EditBox:GetType()) then
-        editBox:SetColors (
+        editBox:SetColors(
           textColor or Colors.LabelText,
           textureColor or Colors.ParentFrame,
           borderColor or Colors.ScrollFrame
         )
       end
-    end
-  end
-
-  function Functions:Refresh()
-    DFL.Frame.Functions.Refresh(self)
-
-    -- Refresh value of edit boxes
-    for k, v in pairs(self:GetChildren()) do
-      local editBox = v._editBox
-      if editBox then
-        editBox:SetText(DejunkDB:Get(editBox._svKey))
-      end
-    end
-  end
-end
-
--- ============================================================================
--- Scripts
--- ============================================================================
-
-do
-  local Scripts = CurrencyField.Scripts
-
-  function Scripts:OnEditFocusGained()
-    self:HighlightText()
-  end
-
-  function Scripts:OnEditFocusLost()
-    self:HighlightText(0, 0)
-    self:SetText(DejunkDB:Get(self._svKey))
-  end
-
-  function Scripts:OnTextChanged()
-    DejunkDB:Set(self._svKey, floor(abs(self:GetNumber())))
-  end
-
-  function Scripts:OnTabPressed()
-    if (IsShiftKeyDown()) then
-      self._prevEditBox:SetFocus()
-    else
-      self._nextEditBox:SetFocus()
     end
   end
 end
