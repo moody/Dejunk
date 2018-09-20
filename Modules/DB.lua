0a4061abec4254feee8ae1dafcfbd2cd858f5f8f
moody <dethanyel@gmail.com>
2018-09-20 13:44:28 -0400

    updated DDBL version

--- Modules/DB.lua
+++ Modules/DB.lua
@@ -127,7 +127,7 @@ end
 -- Initializes the database.
 function DB:Initialize()
   self.Initialize = nil
-  local db = DethsLibLoader("DethsDBLib", "1.0"):Create(AddonName, defaults)
+  local db = DethsLibLoader("DethsDBLib", "1.1"):Create(AddonName, defaults)
   setmetatable(self, {__index = db})
   if not db:ProfileExists("Global") then db:CreateProfile("Global") end
   reformat()
