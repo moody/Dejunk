73f49524ff1911071571cdf3841d729df3a95823
moody <dethanyel@gmail.com>
2019-03-22 10:18:54 -0400

    ignore unlocalized

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -5,4 +5,4 @@ local AddonName, AddonTable = ...
 local L = LibStub('AceLocale-3.0'):NewLocale(AddonName, 'deDE')
 if not L then return end
 
---@localization(locale="deDE", format="lua_additive_table")@
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
