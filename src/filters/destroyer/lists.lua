bb24399f09576d17d7b7056e3589e526bbd4918a
Justin Moody <25038653+moody@users.noreply.github.com>
2020-01-16 16:07:44 -0500

    ignore Exclusions first when destroying

--- src/filters/destroyer/lists.lua
+++ src/filters/destroyer/lists.lua
@@ -1,26 +1,22 @@
 local _, Addon = ...
 local DB = Addon.DB
+local Destroyables = Addon.Lists.Destroyables
+local Exclusions = Addon.Lists.Exclusions
 local Filter = {}
+local Inclusions = Addon.Lists.Inclusions
 local L = Addon.Libs.L
-local Lists = Addon.Lists
 
 function Filter:Run(item)
-  if Lists.Destroyables:Has(item.ItemID) then
+  if DB.Profile.DestroyIgnoreExclusions and Exclusions:Has(item.ItemID) then
+    return "NOT_JUNK", L.REASON_DESTROY_IGNORE_EXCLUSIONS_TEXT
+  end
+
+  if Destroyables:Has(item.ItemID) then
     return "JUNK", L.REASON_ITEM_ON_LIST_TEXT:format(L.DESTROYABLES_TEXT)
   end
 
-  if
-    DB.Profile.DestroyInclusions and
-    Lists.Inclusions:Has(item.ItemID)
-  then
+  if DB.Profile.DestroyInclusions and Inclusions:Has(item.ItemID) then
     return "JUNK", L.REASON_DESTROY_INCLUSIONS_TEXT
-  end
-
-  if
-    DB.Profile.DestroyIgnoreExclusions and
-    Lists.Exclusions:Has(item.ItemID)
-  then
-    return "NOT_JUNK", L.REASON_DESTROY_IGNORE_EXCLUSIONS_TEXT
   end
 
   return "PASS"
