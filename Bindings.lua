21cdd15859b05001719d8fb4ef9b4030318b3812
Justin Moody <25038653+moody@users.noreply.github.com>
2020-01-17 12:01:53 -0500

    add bindings for Undestroyables + improve code

--- Bindings.lua
+++ Bindings.lua
@@ -1,5 +1,3 @@
--- Bindings: sets up binding data and functions.
-
 local AddonName, Addon = ...
 local _G = _G
 local Colors = Addon.Colors
@@ -11,38 +9,41 @@ local Inclusions = Addon.Lists.Inclusions
 local L = Addon.Libs.L
 local Tools = Addon.Tools
 local UI = Addon.UI
+local Undestroyables = Addon.Lists.Undestroyables
+local Lists = Addon.Lists
 
 -- Variables
 local currentItemID = nil
 
+-- ============================================================================
+-- Binding Strings
+-- ============================================================================
+
 -- Category
-_G["BINDING_CATEGORY_DEJUNK"] = DCL:ColorString(AddonName, Colors.Primary)
+_G.BINDING_CATEGORY_DEJUNK = DCL:ColorString(AddonName, Colors.Primary)
 
--- Blank headers
-_G["BINDING_HEADER_DEJUNKBLANK1"] = ""
-_G["BINDING_HEADER_DEJUNKBLANK2"] = ""
-_G["BINDING_HEADER_DEJUNKBLANK3"] = ""
-
--- Toggle options
-_G["BINDING_NAME_DEJUNK_TOGGLE_OPTIONS"] = L.BINDINGS_TOGGLE_OPTIONS_TEXT
-
--- Start destroying
-_G["BINDING_NAME_DEJUNK_START_DESTROYING"] = L.START_DESTROYING_BUTTON_TEXT
+-- General
+_G.BINDING_NAME_DEJUNK_TOGGLE_OPTIONS = L.BINDINGS_TOGGLE_OPTIONS_TEXT
+_G.BINDING_NAME_DEJUNK_START_DESTROYING = L.START_DESTROYING_BUTTON_TEXT
 
 -- Inclusions
-_G["BINDING_NAME_DEJUNK_ADD_INCLUSIONS"] = L.BINDINGS_ADD_TO_LIST_TEXT:format(Inclusions.localeColored)
-_G["BINDING_NAME_DEJUNK_REM_INCLUSIONS"] = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Inclusions.localeColored)
+_G.BINDING_NAME_DEJUNK_ADD_INCLUSIONS = L.BINDINGS_ADD_TO_LIST_TEXT:format(Inclusions.localeColored)
+_G.BINDING_NAME_DEJUNK_REM_INCLUSIONS = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Inclusions.localeColored)
 
 -- Exclusions
-_G["BINDING_NAME_DEJUNK_ADD_EXCLUSIONS"] = L.BINDINGS_ADD_TO_LIST_TEXT:format(Exclusions.localeColored)
-_G["BINDING_NAME_DEJUNK_REM_EXCLUSIONS"] = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Exclusions.localeColored)
+_G.BINDING_NAME_DEJUNK_ADD_EXCLUSIONS = L.BINDINGS_ADD_TO_LIST_TEXT:format(Exclusions.localeColored)
+_G.BINDING_NAME_DEJUNK_REM_EXCLUSIONS = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Exclusions.localeColored)
 
 -- Destroyables
-_G["BINDING_NAME_DEJUNK_ADD_DESTROYABLES"] = L.BINDINGS_ADD_TO_LIST_TEXT:format(Destroyables.localeColored)
-_G["BINDING_NAME_DEJUNK_REM_DESTROYABLES"] = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Destroyables.localeColored)
+_G.BINDING_NAME_DEJUNK_ADD_DESTROYABLES = L.BINDINGS_ADD_TO_LIST_TEXT:format(Destroyables.localeColored)
+_G.BINDING_NAME_DEJUNK_REM_DESTROYABLES = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Destroyables.localeColored)
+
+-- Undestroyables
+_G.BINDING_NAME_DEJUNK_ADD_UNDESTROYABLES = L.BINDINGS_ADD_TO_LIST_TEXT:format(Undestroyables.localeColored)
+_G.BINDING_NAME_DEJUNK_REM_UNDESTROYABLES = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Undestroyables.localeColored)
 
 -- ============================================================================
--- General Bindings
+-- Binding Functions
 -- ============================================================================
 
 function DejunkBindings_ToggleOptions()
@@ -53,41 +54,14 @@ function DejunkBindings_StartDestroying()
   Destroyer:StartDestroying()
 end
 
--- ============================================================================
--- List Bindings
--- ============================================================================
-
--- Inclusions
-function DejunkBindings_AddToInclusions()
+function DejunkBindings_AddToList(listName)
   if not currentItemID then return end
-  Inclusions:Add(currentItemID)
+  Lists[listName]:Add(currentItemID)
 end
 
-function DejunkBindings_RemoveFromInclusions()
+function DejunkBindings_RemoveFromList(listName)
   if not currentItemID then return end
-  Inclusions:Remove(currentItemID, true)
-end
-
--- Exclusions
-function DejunkBindings_AddToExclusions()
-  if not currentItemID then return end
-  Exclusions:Add(currentItemID)
-end
-
-function DejunkBindings_RemoveFromExclusions()
-  if not currentItemID then return end
-  Exclusions:Remove(currentItemID, true)
-end
-
--- Destroyables
-function DejunkBindings_AddToDestroyables()
-  if not currentItemID then return end
-  Destroyables:Add(currentItemID)
-end
-
-function DejunkBindings_RemoveFromDestroyables()
-  if not currentItemID then return end
-  Destroyables:Remove(currentItemID, true)
+  Lists[listName]:Remove(currentItemID, true)
 end
 
 -- ============================================================================
