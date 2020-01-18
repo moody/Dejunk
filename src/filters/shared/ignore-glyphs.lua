56b1f2b1a4a96e19ad8f487f19add72b6ff74253
Justin Moody <25038653+moody@users.noreply.github.com>
2020-01-18 13:06:56 -0500

    set Ignore Glyphs to ignore items of poor quality

--- src/filters/shared/ignore-glyphs.lua
+++ src/filters/shared/ignore-glyphs.lua
@@ -4,11 +4,16 @@ if not Addon.IS_RETAIL then return end
 local Consts = Addon.Consts
 local DB = Addon.DB
 local L = Addon.Libs.L
+local LE_ITEM_QUALITY_POOR = _G.LE_ITEM_QUALITY_POOR
 
 -- Dejunker
 Addon.Filters:Add(Addon.Dejunker, {
   Run = function(_, item)
-    if DB.Profile.IgnoreGlyphs and item.Class == Consts.GLYPH_CLASS then
+    if
+      DB.Profile.IgnoreGlyphs and
+      item.Class == Consts.GLYPH_CLASS and
+      item.Quality ~= LE_ITEM_QUALITY_POOR
+    then
       return "NOT_JUNK", L.REASON_IGNORE_GLYPHS_TEXT
     end
 
@@ -19,7 +24,11 @@ Addon.Filters:Add(Addon.Dejunker, {
 -- Destroyer
 Addon.Filters:Add(Addon.Destroyer, {
   Run = function(_, item)
-    if DB.Profile.DestroyIgnoreGlyphs and item.Class == Consts.GLYPH_CLASS then
+    if
+      DB.Profile.DestroyIgnoreGlyphs and
+      item.Class == Consts.GLYPH_CLASS and
+      item.Quality ~= LE_ITEM_QUALITY_POOR
+    then
       return "NOT_JUNK", L.REASON_IGNORE_GLYPHS_TEXT
     end
 
