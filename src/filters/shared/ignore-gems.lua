7ca8ff348da79d4084d175e62881a940d8fc73a7
Justin Moody <25038653+moody@users.noreply.github.com>
2020-01-18 13:06:11 -0500

    set Ignore Gems to ignore items of poor quality

--- src/filters/shared/ignore-gems.lua
+++ src/filters/shared/ignore-gems.lua
@@ -4,11 +4,16 @@ if not Addon.IS_RETAIL then return end
 local Consts = Addon.Consts
 local DB = Addon.DB
 local L = Addon.Libs.L
+local LE_ITEM_QUALITY_POOR = _G.LE_ITEM_QUALITY_POOR
 
 -- Dejunker
 Addon.Filters:Add(Addon.Dejunker, {
   Run = function(_, item)
-    if DB.Profile.IgnoreGems and item.Class == Consts.GEM_CLASS then
+    if
+      DB.Profile.IgnoreGems and
+      item.Class == Consts.GEM_CLASS and
+      item.Quality ~= LE_ITEM_QUALITY_POOR
+    then
       return "NOT_JUNK", L.REASON_IGNORE_GEMS_TEXT
     end
 
@@ -19,7 +24,11 @@ Addon.Filters:Add(Addon.Dejunker, {
 -- Destroyer
 Addon.Filters:Add(Addon.Destroyer, {
   Run = function(_, item)
-    if DB.Profile.DestroyIgnoreGems and item.Class == Consts.GEM_CLASS then
+    if
+      DB.Profile.DestroyIgnoreGems and
+      item.Class == Consts.GEM_CLASS and
+      item.Quality ~= LE_ITEM_QUALITY_POOR
+    then
       return "NOT_JUNK", L.REASON_IGNORE_GEMS_TEXT
     end
 
