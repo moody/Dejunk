7d927b76dae11eed82535f4cd73cfcbf89e1af99
Justin Moody <25038653+moody@users.noreply.github.com>
2020-01-18 11:05:23 -0500

    fix bug with ignore boe option and recipes

--- src/filters/shared/ignore-binds-when-equipped.lua
+++ src/filters/shared/ignore-binds-when-equipped.lua
@@ -1,4 +1,5 @@
 local _, Addon = ...
+local Consts = Addon.Consts
 local DB = Addon.DB
 local DTL = Addon.Libs.DTL
 local L = Addon.Libs.L
@@ -21,6 +22,7 @@ Addon.Filters:Add(Addon.Dejunker, {
   Run = function(_, item)
     if
       DB.Profile.IgnoreBindsWhenEquipped and
+      item.Class ~= Consts.RECIPE_CLASS and
       item.Quality ~= LE_ITEM_QUALITY_POOR
     then
       return isBindsWhenEquipped(item)
@@ -35,6 +37,7 @@ Addon.Filters:Add(Addon.Destroyer, {
   Run = function(_, item)
     if
       DB.Profile.DestroyIgnoreBindsWhenEquipped and
+      item.Class ~= Consts.RECIPE_CLASS and
       item.Quality ~= LE_ITEM_QUALITY_POOR
     then
       return isBindsWhenEquipped(item)
