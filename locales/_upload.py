40c88c0d5ca7a891b72f4e6c0378ce5170d8792a
Justin Moody <25038653+moody@users.noreply.github.com>
2020-02-23 20:31:47 -0500

    fix payload + print messages

--- locales/_upload.py
+++ locales/_upload.py
@@ -1,32 +1,47 @@
+import json
 import os
 import requests
+import sys
 
 
-PROJECT_ID = "257636"
 API_KEY = os.getenv("CF_API_KEY")
-
 assert API_KEY
 
+LOCALE_ENTRY_PREFIX = "L["
+PROJECT_ID = "257636"
 API_ENDPOINT = f"https://wow.curseforge.com/api/projects/{PROJECT_ID}/localization/import?token={API_KEY}"
 
 
 # Get localizations
+print("Retrieving locale entries...")
 localizations = ""
 
 with open("locales/enUS.lua") as f:
     entries = []
 
     for line in f.readlines():
-        if "L[" in line:
+        if line.startswith(LOCALE_ENTRY_PREFIX):
+            print(f"  {line.split('=')[0]}")
             entries.append(line)
 
     localizations = "".join(entries)
+    print(f"\nRetrieved {len(entries)} locale entries.\n")
 
 
-# POST to CurseForge
+# Upload to CurseForge
+print("Uploading to CurseForge...")
+
 payload = {
-    "metadata": {"language": "enUS", "missing-phrase-handling": "DeletePhrase"},
+    "metadata": json.dumps(
+        {"language": "enUS", "missing-phrase-handling": "DeletePhrase"}
+    ),
     "localizations": localizations,
 }
 
-requests.post(url=API_ENDPOINT, data=payload)
+r = requests.post(url=API_ENDPOINT, data=payload)
+
+if r.ok:
+    print("Success!")
+else:
+    print(r.text)
+    sys.exit(1)
