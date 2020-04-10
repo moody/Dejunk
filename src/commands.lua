efb2703bc012565938517b966a5ebbd66a0245a8
Justin Moody <25038653+moody@users.noreply.github.com>
2020-04-10 06:32:57 -0400

    simplify command code, add `destroy` command

--- src/commands.lua
+++ src/commands.lua
@@ -1,5 +1,6 @@
 local _, Addon = ...
 local Commands = Addon.Commands
+local Destroyer = Addon.Destroyer
 local E = Addon.Events
 local EventManager = Addon.EventManager
 local strlower = _G.strlower
@@ -19,8 +20,16 @@ EventManager:Once(E.Wow.PlayerLogin, function()
     local args = {}
     for s in msg:gmatch('%S+') do args[#args+1] = s end
 
-    -- Parse
-    Commands:Parse(args)
+    -- First arg is command name
+    local cmd = table.remove(args, 1)
+    local func = cmd and Commands[cmd] or nil
+
+    -- Execute command
+    if type(func) == "function" then
+      func(args)
+    else
+      UI:Toggle()
+    end
   end
 end)
 
@@ -28,18 +37,7 @@ end)
 -- Functions
 -- ============================================================================
 
-function Commands:Parse(args)
-  -- First arg is command name
-  local cmd = table.remove(args, 1)
-  local func = self.Default
-
-  -- TODO: add commands
-
-  -- Execute command
-  func(self, args)
-end
-
--- Default command.
-function Commands:Default()
-  UI:Toggle()
+-- `/dejunk destroy`
+function Commands.destroy()
+  Destroyer:Start()
 end
