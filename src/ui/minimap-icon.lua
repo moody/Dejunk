bb40527fbedbf020cfa0e0b411422d895e2f7317
Justin Moody <25038653+moody@users.noreply.github.com>
2020-11-18 11:45:23 -0500

    update minimap icon

--- src/ui/minimap-icon.lua
+++ src/ui/minimap-icon.lua
@@ -2,9 +2,12 @@ local AddonName, Addon = ...
 local Colors = Addon.Colors
 local DB = Addon.DB
 local DCL = Addon.Libs.DCL
+local Dejunker = Addon.Dejunker
 local Destroyer = Addon.Destroyer
 local E = Addon.Events
 local EventManager = Addon.EventManager
+local IsShiftKeyDown = _G.IsShiftKeyDown
+local ItemWindow = Addon.UI.ItemWindow
 local L = Addon.Libs.L
 local LDB = Addon.Libs.LDB
 local LDBIcon = Addon.Libs.LDBIcon
@@ -19,10 +22,16 @@ EventManager:Once(E.DatabaseReady, function()
   	icon = "Interface\\AddOns\\Dejunk\\Dejunk_Icon",
 
     OnClick = function(_, button)
-      if (button == "LeftButton") then
+      if IsShiftKeyDown() then
+        if (button == "LeftButton") then
+          ItemWindow:Toggle(Dejunker)
+        end
+
+        if (button == "RightButton") then
+          ItemWindow:Toggle(Destroyer)
+        end
+      elseif (button == "LeftButton") then
         UI:Toggle()
-      elseif (button == "RightButton") then
-        Destroyer:Start()
       end
     end,
 
@@ -32,8 +41,11 @@ EventManager:Once(E.DatabaseReady, function()
         Addon.VERSION
       )
       tooltip:AddLine(" ")
-			tooltip:AddLine(DCL:ColorString(L.MINIMAP_ICON_TOOLTIP_1, DCL.CSS.White))
-      tooltip:AddLine(DCL:ColorString(L.MINIMAP_ICON_TOOLTIP_2, DCL.CSS.White))
+      for i=1, 3 do
+        local left = L["MINIMAP_ICON_TOOLTIP_"..i.."_L"]
+        local right = L["MINIMAP_ICON_TOOLTIP_"..i.."_R"]
+        tooltip:AddDoubleLine(left, right, 1.0, 0.82, 0, 1, 1, 1)
+      end
 		end,
   })
 
