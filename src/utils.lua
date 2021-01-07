e5befc6932be31582c9f54c37d9e5d8fe13564d4
Justin Moody <25038653+moody@users.noreply.github.com>
2021-01-06 21:29:16 -0500

    move isEquipmentItem() to Utils

--- src/utils.lua
+++ src/utils.lua
@@ -4,6 +4,10 @@ local DCL = Addon.Libs.DCL
 local GetContainerItemPurchaseInfo = _G.GetContainerItemPurchaseInfo
 local ItemQuality = Addon.ItemQuality
 local L = Addon.Libs.L
+local LE_ITEM_ARMOR_COSMETIC = _G.LE_ITEM_ARMOR_COSMETIC
+local LE_ITEM_ARMOR_GENERIC = _G.LE_ITEM_ARMOR_GENERIC
+local LE_ITEM_WEAPON_FISHINGPOLE = _G.LE_ITEM_WEAPON_FISHINGPOLE
+local LE_ITEM_WEAPON_GENERIC = _G.LE_ITEM_WEAPON_GENERIC
 local StaticPopup_Show = _G.StaticPopup_Show
 local Utils = Addon.Utils
 
@@ -32,6 +36,38 @@ end
 -- @return - the item id, or nil
 function Utils:GetItemIDFromLink(itemLink)
   return (itemLink and itemLink:match("item:(%d+)")) or nil
+end
+
+do -- Utils:IsEquipmentItem()
+  -- Special check required for these generic armor types.
+  local SPECIAL_ARMOR_EQUIPSLOTS = {
+    ["INVTYPE_FINGER"] = true,
+    ["INVTYPE_NECK"] = true,
+    ["INVTYPE_TRINKET"] = true,
+    ["INVTYPE_HOLDABLE"] = true
+  }
+
+  -- Returns true if the item is an equippable item; excluding generic
+  -- armor/weapon types, cosmetic items, and fishing poles.
+  -- @return {boolean}
+  function Utils:IsEquipmentItem(item)
+    if item.Class == Consts.ARMOR_CLASS then
+      if SPECIAL_ARMOR_EQUIPSLOTS[item.EquipSlot] then return true end
+      local armorType = Consts.ARMOR_SUBCLASSES[item.SubClass]
+      return (
+        armorType ~= LE_ITEM_ARMOR_GENERIC and
+        armorType ~= LE_ITEM_ARMOR_COSMETIC
+      )
+    elseif item.Class == Consts.WEAPON_CLASS then
+      local weaponType = Consts.WEAPON_SUBCLASSES[item.SubClass]
+      return (
+        weaponType ~= LE_ITEM_WEAPON_GENERIC and
+        weaponType ~= LE_ITEM_WEAPON_FISHINGPOLE
+      )
+    end
+
+    return false
+  end
 end
 
 -- Returns true if the specified item can be sold.
