53113fd719d8c64e1a63b198ef7eae4cfa63048f
Justin Moody <25038653+moody@users.noreply.github.com>
2021-01-13 09:06:27 -0500

    remove tests

--- test/_init.lua
+++
@@ -1,9 +0,0 @@
-if not _G.__WEST_LIB__ then return end
-
-local _, Addon = ...
-local west = _G.__WEST_LIB__()
-Addon.West = west
-
--- Register slash command
-_G.SLASH_DEJUNKTEST1 = "/dejunktest"
-_G.SlashCmdList.DEJUNKTEST = west.run
