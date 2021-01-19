828e5efe2b343f2af67b0e7e4be2334fd71d5f94
Justin Moody <25038653+moody@users.noreply.github.com>
2021-01-19 16:56:05 -0500

    update bindings

--- Bindings.lua
+++ Bindings.lua
@@ -18,9 +18,11 @@ local currentItemID = nil
 _G.BINDING_CATEGORY_DEJUNK = DCL:ColorString(AddonName, Colors.Primary)
 
 -- Headers.
+for i=1, 3 do _G["BINDING_HEADER_DEJUNK_HEADER_BLANK"..i] = "" end
 _G.BINDING_HEADER_DEJUNK_HEADER_GENERAL = L.GENERAL_TEXT
 _G.BINDING_HEADER_DEJUNK_HEADER_SELL = L.SELL_TEXT
 _G.BINDING_HEADER_DEJUNK_HEADER_DESTROY = L.DESTROY_TEXT
+_G.BINDING_HEADER_DEJUNK_HEADER_LISTS = L.LISTS_TEXT
 
 -- General.
 _G.BINDING_NAME_DEJUNK_TOGGLE_OPTIONS_FRAME = L.TOGGLE_OPTIONS_FRAME
@@ -31,27 +33,27 @@ _G.BINDING_NAME_DEJUNK_OPEN_LOOTABLES = L.OPEN_LOOTABLES
 -- Sell.
 _G.BINDING_NAME_DEJUNK_START_SELLING = L.START_SELLING_BUTTON_TEXT
 _G.BINDING_NAME_DEJUNK_SELL_NEXT_ITEM = L.SELL_NEXT_ITEM
-_G.BINDING_NAME_DEJUNK_ADD_INCLUSIONS =
-  L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.inclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_REM_INCLUSIONS =
-  L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.inclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_ADD_EXCLUSIONS =
-  L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.exclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_REM_EXCLUSIONS =
-  L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.exclusions.profile.localeShort)
+_G.BINDING_NAME_DEJUNK_ADD_INCLUSIONS = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.inclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_REM_INCLUSIONS = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.inclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_ADD_EXCLUSIONS = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.exclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_REM_EXCLUSIONS = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.exclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_ADD_INCLUSIONS_GLOBAL = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.inclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_REM_INCLUSIONS_GLOBAL = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.inclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_ADD_EXCLUSIONS_GLOBAL = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.sell.exclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_REM_EXCLUSIONS_GLOBAL = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.sell.exclusions.global.locale)
 
 -- Destroy.
 _G.BINDING_NAME_DEJUNK_START_DESTROYING = L.START_DESTROYING
 _G.BINDING_NAME_DEJUNK_DESTROY_NEXT_ITEM = L.DESTROY_NEXT_ITEM
 _G.BINDING_NAME_DEJUNK_DESTROY_ALL_ITEMS = L.DESTROY_ALL_ITEMS
-_G.BINDING_NAME_DEJUNK_ADD_DESTROYABLES =
-  L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.inclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_REM_DESTROYABLES =
-  L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.inclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_ADD_UNDESTROYABLES =
-  L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.exclusions.profile.localeShort)
-_G.BINDING_NAME_DEJUNK_REM_UNDESTROYABLES =
-  L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.exclusions.profile.localeShort)
+_G.BINDING_NAME_DEJUNK_ADD_DESTROYABLES = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.inclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_REM_DESTROYABLES = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.inclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_ADD_UNDESTROYABLES = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.exclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_REM_UNDESTROYABLES = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.exclusions.profile.locale)
+_G.BINDING_NAME_DEJUNK_ADD_DESTROYABLES_GLOBAL = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.inclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_REM_DESTROYABLES_GLOBAL = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.inclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_ADD_UNDESTROYABLES_GLOBAL = L.BINDINGS_ADD_TO_LIST_TEXT:format(Lists.destroy.exclusions.global.locale)
+_G.BINDING_NAME_DEJUNK_REM_UNDESTROYABLES_GLOBAL = L.BINDINGS_REMOVE_FROM_LIST_TEXT:format(Lists.destroy.exclusions.global.locale)
 
 -- ============================================================================
 -- Binding Functions
@@ -92,13 +94,11 @@ end
 -- Item Tooltip Hook
 -- ============================================================================
 
-local function OnTooltipSetItem(self, ...)
+_G.GameTooltip:HookScript("OnTooltipSetItem", function(self, ...)
   currentItemID = Utils:GetItemIDFromLink(select(2, self:GetItem()))
-end
+end)
 
-local function OnTooltipCleared(self, ...)
+_G.GameTooltip:HookScript("OnTooltipCleared", function(self, ...)
   currentItemID = nil
-end
+end)
 
-_G.GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
-_G.GameTooltip:HookScript("OnTooltipCleared", OnTooltipCleared)
