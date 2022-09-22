ac6e332b32ffa1b0d02427c56a467d914d19faf5
moody <25038653+moody@users.noreply.github.com>
2022-09-22 17:08:22 -0400

    remove old files

--- locales/deDE.lua
+++
@@ -1,7 +0,0 @@
--- Dejunk: deDE (German) localization file.
-
-local AddonName = ...
-local L = _G.LibStub('AceLocale-3.0'):NewLocale(AddonName, 'deDE')
-if not L then return end
-
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
--- locales/koKR.lua
+++
@@ -1,7 +0,0 @@
--- Dejunk: koKR (Korean) localization file.
-
-local AddonName = ...
-local L = _G.LibStub('AceLocale-3.0'):NewLocale(AddonName, 'koKR')
-if not L then return end
-
---@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
