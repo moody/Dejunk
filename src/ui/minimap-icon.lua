208069da602ac20d84c836c343c0e04cfb4f9dc1
moody <25038653+moody@users.noreply.github.com>
2022-09-25 16:21:49 -0400

    update minimap icon

--- src/ui/minimap-icon.lua
+++ src/ui/minimap-icon.lua
@@ -1,13 +1,12 @@
 local ADDON_NAME, Addon = ...
 local Colors = Addon.Colors
-local Destroyer = Addon.Destroyer
+local Commands = Addon.Commands
 local E = Addon.Events
 local EventManager = Addon.EventManager
 local L = Addon.Locale
 local LDB = Addon.Libs.LDB
 local LDBIcon = Addon.Libs.LDBIcon
 local SavedVariables = Addon.SavedVariables
-local UserInterface = Addon.UserInterface
 
 EventManager:Once(E.SavedVariablesReady, function()
   local object = LDB:NewDataObject(ADDON_NAME, {
@@ -17,18 +16,23 @@ EventManager:Once(E.SavedVariablesReady, function()
 
     OnClick = function(_, button)
       if button == "LeftButton" then
-        UserInterface:Toggle()
+        Commands.options()
       end
 
       if button == "RightButton" then
-        Destroyer:Start()
+        if IsShiftKeyDown() then
+          Commands.destroy()
+        else
+          Commands.junk()
+        end
       end
     end,
 
     OnTooltipShow = function(tooltip)
       tooltip:AddDoubleLine(Colors.Blue(ADDON_NAME), Addon.VERSION)
-      tooltip:AddDoubleLine(L.LEFT_CLICK, Colors.White(L.TOGGLE_USER_INTERFACE))
-      tooltip:AddDoubleLine(L.RIGHT_CLICK, Colors.White(L.DESTROY_NEXT_ITEM))
+      tooltip:AddDoubleLine(L.LEFT_CLICK, Colors.White(L.TOGGLE_OPTIONS_FRAME))
+      tooltip:AddDoubleLine(L.RIGHT_CLICK, Colors.White(L.TOGGLE_JUNK_FRAME))
+      tooltip:AddDoubleLine(L.SHIFT_RIGHT_CLICK, Colors.White(L.DESTROY_NEXT_ITEM))
     end
   })
   LDBIcon:Register(ADDON_NAME, object, SavedVariables:GetGlobal().minimapIcon)
