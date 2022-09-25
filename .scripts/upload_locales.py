be9f25430781d00a3009775f086bf06f38d94373
moody <25038653+moody@users.noreply.github.com>
2022-09-25 18:00:50 -0400

    update upload script

--- .scripts/upload_locales.py
+++ .scripts/upload_locales.py
@@ -1,13 +1,11 @@
 import json
 import os
 import requests
-import sys
 
 API_KEY = os.getenv("CF_API_KEY")
 assert API_KEY
 
 API_ENDPOINT = "https://wow.curseforge.com/api/projects/413260/localization"
-EXPORT_URL = f"{API_ENDPOINT}/export?token={API_KEY}"
 IMPORT_URL = f"{API_ENDPOINT}/import?token={API_KEY}"
 IMPORT_METADATA = json.dumps(
     {"language": "enUS", "missing-phrase-handling": "DeletePhrase"}
@@ -18,22 +16,14 @@ def parse(lines):
     entries = []
     for line in lines:
         line = line.strip()
-        if line.startswith("L["):
+        if line.startswith("L."):
             entries.append(line)
     return entries
 
 
 def getEntries():
-    with open("locales/enUS.lua", encoding="utf-8") as f:
+    with open("src/locale.lua", encoding="utf-8") as f:
         return parse(f.readlines())
-
-
-def getCurseEntries():
-    r = requests.get(url=EXPORT_URL)
-    if r.ok:
-        return parse(r.text.splitlines())
-    else:
-        raise Exception(r.text)
 
 
 def upload(localizations):
@@ -49,25 +39,12 @@ def upload(localizations):
 
 
 # Get entries.
-print("Retrieving local entries...", end=" ")
+print("Retrieving entries...", end=" ")
 entries = getEntries()
 print(f"{len(entries)} entries retrieved.")
 
-# Get entries from Curse.
-print("Retrieving Curse entries...", end=" ")
-curseEntries = getCurseEntries()
-print(f"{len(curseEntries)} entries retrieved.")
-
-# Get unchanged entries.
-unchangedEntries = [x for x in entries if x in curseEntries]
-
-# Upload [unchangedEntries], so that stale entries are deleted.
-print(f"Uploading {len(unchangedEntries)} unchanged entries...", end=" ")
-upload(unchangedEntries)
-print("Done.")
-
-# Upload all entries.
-print(f"Uploading {len(entries)} local entries:")
+# Upload entries.
+print(f"Uploading {len(entries)} entries:")
 for k in entries:
     print(f"  {k}")
 upload(entries)
