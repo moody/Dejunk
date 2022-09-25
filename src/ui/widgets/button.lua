09d74f1d06829361b1d005c3f81be4517d7635fc
moody <25038653+moody@users.noreply.github.com>
2022-09-25 15:13:22 -0400

    update button

--- src/ui/widgets/button.lua
+++ src/ui/widgets/button.lua
@@ -11,35 +11,45 @@ local Colors = Addon.Colors
     points? = table[],
     width? = number,
     labelText = string,
+    labelColor? = Color,
     onClick? = function(self, button) -> nil
   }
 ]]
 function Widgets:Button(options)
   -- Defaults.
   options.frameType = "Button"
+  options.labelColor = options.labelColor or Colors.Gold
 
   -- Base frame.
   local frame = self:Frame(options)
-  frame:SetBackdropColor(Colors.DarkGrey:GetRGBA(0.8))
+  frame:SetBackdropColor(Colors.DarkGrey:GetRGBA(0.75))
+  frame:SetBackdropBorderColor(0, 0, 0, 1)
 
   -- Label text.
   frame.label = frame:CreateFontString("$parent_Label", "ARTWORK", "GameFontNormal")
-  frame.label:SetText(Colors.White(options.labelText))
+  frame.label:SetText(options.labelText)
+  frame.label:SetTextColor(options.labelColor:GetRGB())
   frame.label:SetPoint("LEFT", frame, self:Padding(0.5), 0)
   frame.label:SetPoint("RIGHT", frame, -self:Padding(0.5), 0)
   frame.label:SetWordWrap(false)
   frame:SetFontString(frame.label)
 
-  frame:SetHeight(max(frame.label:GetHeight(), frame.label:GetStringHeight()) + self:Padding(2))
-
   frame:SetScript("OnClick", options.onClick)
 
   frame:SetScript("OnEnter", function(self)
-    self:SetBackdropColor(Colors.DarkGrey:GetRGB())
+    self:SetBackdropColor(options.labelColor:GetRGBA(0.25))
+    self:SetBackdropBorderColor(options.labelColor:GetRGB())
+    self.label:SetTextColor(Colors.White:GetRGB())
   end)
 
   frame:SetScript("OnLeave", function(self)
-    self:SetBackdropColor(Colors.DarkGrey:GetRGBA(0.8))
+    self:SetBackdropColor(Colors.DarkGrey:GetRGBA(0.75))
+    self:SetBackdropBorderColor(0, 0, 0, 1)
+    self.label:SetTextColor(options.labelColor:GetRGB())
+  end)
+
+  frame:SetScript("OnUpdate", function(self)
+    self:SetHeight(self.label:GetHeight() + Widgets:Padding(2))
   end)
 
   return frame
