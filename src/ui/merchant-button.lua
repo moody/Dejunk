7687d028699c3d5f65d3869d24275b3ef9e4891b
moody <25038653+moody@users.noreply.github.com>
2022-11-05 02:58:17 -0400

    add elvui support for merchant button

--- src/ui/merchant-button.lua
+++ src/ui/merchant-button.lua
@@ -7,20 +7,26 @@ local L = Addon.Locale
 local SavedVariables = Addon.SavedVariables
 local Tooltip = Addon.Tooltip
 
-EventManager:Once(E.SavedVariablesReady, function()
+EventManager:Once(E.Wow.MerchantShow, function()
   local frame = CreateFrame("Button", ADDON_NAME .. "_MerchantButton", MerchantFrame, "UIPanelButtonTemplate")
-  frame:SetText(ADDON_NAME)
-  frame:SetWidth(frame:GetTextWidth() + 32)
-  frame:SetHeight(frame:GetTextHeight() + 12)
-  frame:SetPoint("TOPLEFT", 60, -28)
   frame:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+  frame:SetText(ADDON_NAME)
+  frame:SetWidth(90)
+  frame:SetHeight(22)
+  frame:SetPoint("TOPLEFT", 60, -28)
+
+  -- Skin for ElvUI.
+  pcall(function(frame)
+    local E = ElvUI[1]
+    local S = E:GetModule("Skins")
+    if E.private.skins.blizzard.enable and E.private.skins.blizzard.merchant then
+      S:HandleButton(frame)
+      frame:SetPoint("TOPLEFT", 4, -4)
+    end
+  end, frame)
 
   MerchantFrame:HookScript("OnUpdate", function()
-    if SavedVariables:Get().merchantButton then
-      frame:Show()
-    else
-      frame:Hide()
-    end
+    if SavedVariables:Get().merchantButton then frame:Show() else frame:Hide() end
   end)
 
   frame:HookScript("OnUpdate", function(self)
