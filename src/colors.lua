2390351eff70b5b8859c019a3398da64f6e8f817
moody <moody@users.noreply.github.com>
2024-05-01 19:37:25 -0400

    refactor colors

--- src/colors.lua
+++ src/colors.lua
@@ -1,21 +1,19 @@
 local _, Addon = ...
-local Colors = Addon:GetModule("Colors")
+local Colors = Addon:GetModule("Colors") ---@class Colors
 
-local colors = {
-  White = "FFFFFFFF",
-  Blue = "FF4FAFE3",
-  Red = "FFE34F4F",
-  Green = "FF4FE34F",
-  Yellow = "FFE3E34F",
-  Gold = "FFFFD100",
-  Grey = "FF9D9D9D",
-  DarkGrey = "FF1E1E1E"
-}
+-- =============================================================================
+-- Local Functions
+-- =============================================================================
 
-for name, hex in pairs(colors) do
+--- Creates a color from the given AARRGGBB hex string.
+--- @param hex string AARRGGBB
+--- @return Color
+local function createColor(hex)
   local color = CreateColorFromHexString(hex)
 
-  local t = setmetatable({}, {
+  --- @class Color
+  --- @overload fun(text: string): string
+  local wrapper = setmetatable({}, {
     __call = function(self, text, alpha)
       alpha = (alpha or 1) * 255
       local _hex = ("%.2x%.2x%.2x%.2x"):format(alpha, color:GetRGBAsBytes())
@@ -23,19 +21,40 @@ for name, hex in pairs(colors) do
     end
   })
 
-  function t:GetRGB()
+  --- Returns the color's normalized RGB values.
+  --- @return number r, number g, number b
+  function wrapper:GetRGB()
     return color:GetRGB()
   end
 
-  function t:GetRGBA(alpha)
+  --- Returns the color's normalized RGBA values.
+  --- @param alpha number normalized alpha (optional override)
+  --- @return number r, number g, number b, number a
+  function wrapper:GetRGBA(alpha)
     local r, g, b, a = color:GetRGBA()
     return r, g, b, alpha or a
   end
 
-  function t:GetHex(alpha)
+  --- Returns the color as an AARRGGBB hex string.
+  --- @param alpha number normalized alpha
+  --- @return string hex
+  function wrapper:GetHex(alpha)
     alpha = (alpha or 1) * 255
     return ("%.2x%.2x%.2x%.2x"):format(alpha, color:GetRGBAsBytes())
   end
 
-  Colors[name] = t
+  return wrapper
 end
+
+-- =============================================================================
+-- Colors
+-- =============================================================================
+
+Colors.Blue = createColor("FF4FAFE3")
+Colors.DarkGrey = createColor("FF1E1E1E")
+Colors.Gold = createColor("FFFFD100")
+Colors.Green = createColor("FF4FE34F")
+Colors.Grey = createColor("FF9D9D9D")
+Colors.Red = createColor("FFE34F4F")
+Colors.White = createColor("FFFFFFFF")
+Colors.Yellow = createColor("FFE3E34F")
