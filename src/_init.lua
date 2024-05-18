a68e63e31544cbab16e347964becad6fe4a7ffde
moody <moody@users.noreply.github.com>
2024-05-18 01:37:42 -0400

    update annotations

--- src/_init.lua
+++ src/_init.lua
@@ -1,10 +1,10 @@
-local ADDON_NAME = ...
+local ADDON_NAME = ... ---@type string
 
 --- @class Addon
 local Addon = select(2, ...)
 
 -- ============================================================================
--- Consts
+-- Addon - Constants
 -- ============================================================================
 
 Addon.VERSION = C_AddOns.GetAddOnMetadata(ADDON_NAME, "Version")
@@ -13,45 +13,66 @@ Addon.IS_VANILLA = WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
 Addon.IS_CATA = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
 
 -- ============================================================================
--- Functions
+-- Addon - Methods
 -- ============================================================================
 
-do -- Addon:GetModule()
+-- Addon:GetModule()
+do
+  --- @type table<string, table>
   local modules = {}
 
+  --- Gets or creates a module table for the given `key`.
+  --- @generic T
+  --- @param key `T`
+  --- @return T
   function Addon:GetModule(key)
+    --- @cast key +string
     key = key:upper()
     if type(modules[key]) ~= "table" then modules[key] = {} end
     return modules[key]
   end
 end
 
-do -- Addon:GetLibrary()
+-- Addon:GetLibrary()
+do
+  --- @enum (key) LibraryKey
   local libraries = {
     LDB = LibStub("LibDataBroker-1.1"),
     LDBIcon = LibStub("LibDBIcon-1.0")
   }
 
+  --- Returns a library based on the given `key`.
+  --- @param key LibraryKey
   function Addon:GetLibrary(key)
     return libraries[key] or error("Invalid library: " .. key)
   end
 end
 
+--- Returns the full path to a file in the `/assets` folder.
+--- @param fileName string
+--- @return string
 function Addon:GetAsset(fileName)
   return ("Interface\\AddOns\\%s\\assets\\%s"):format(ADDON_NAME, fileName)
 end
 
--- Returns latency in seconds.
+--- Returns the current highest latency value in seconds.
+--- @param minLatency? number
+--- @return number latency value will always be `>= 0.2` seconds
 function Addon:GetLatency(minLatency)
   local _, _, home, world = GetNetStats()
   local latency = max(home, world) * 0.001
   return max(latency, minLatency or 0.2)
 end
 
-do -- Addon:Concat()
-  local Colors = Addon:GetModule("Colors") ---@type Colors
+-- Addon:Concat()
+do
+  local Colors = Addon:GetModule("Colors")
   local cache = {}
 
+  --- Concatenates arguments with a given separator.
+  --- @param sep string
+  --- @param ... string|number
+  --- @return string
   function Addon:Concat(sep, ...)
     for k in pairs(cache) do cache[k] = nil end
     for i = 1, select("#", ...) do cache[#cache + 1] = select(i, ...) end
@@ -59,17 +80,26 @@ do -- Addon:Concat()
   end
 end
 
+--- Returns `value` if it is not nil; otherwise, returns `default`.
+--- @generic T1, T2
+--- @param value? T1
+--- @param default T2
+--- @return T1|T2 value
 function Addon:IfNil(value, default)
   if value == nil then return default end
   return value
 end
 
-do -- Addon:IsBusy()
+-- Addon:IsBusy()
+do
   local Confirmer = Addon:GetModule("Confirmer")
-  local L = Addon:GetModule("Locale") ---@type Locale
-  local ListItemParser = Addon:GetModule("ListItemParser") ---@type ListItemParser
+  local L = Addon:GetModule("Locale")
+  local ListItemParser = Addon:GetModule("ListItemParser")
   local Seller = Addon:GetModule("Seller")
 
+  --- Returns `true` with a reason string if a critical process is active.
+  --- @return boolean isBusy
+  --- @return string? reason
   function Addon:IsBusy()
     if Seller:IsBusy() then return true, L.IS_BUSY_SELLING_ITEMS end
     if ListItemParser:IsBusy() then return true, L.IS_BUSY_UPDATING_LISTS end
@@ -78,21 +108,30 @@ do -- Addon:IsBusy()
   end
 end
 
-do -- Addon:ForcePrint(), Addon:Print(), Addon:Debug()
-  local Colors = Addon:GetModule("Colors") ---@type Colors
-  local StateManager = Addon:GetModule("StateManager") ---@type StateManager
+-- Addon:ForcePrint(), Addon:Print(), Addon:Debug()
+do
+  local Colors = Addon:GetModule("Colors")
+  local StateManager = Addon:GetModule("StateManager")
 
+  --- Forcefully prints the given arguments.
+  --- @param ... any
   function Addon:ForcePrint(...)
     print(Colors.Blue("[" .. ADDON_NAME .. "]"), ...)
   end
 
+  --- Prints the given arguments if chat messages are enabled.
+  --- @param ... any
   function Addon:Print(...)
     if StateManager:GetGlobalState().chatMessages then
       print(Colors.Blue("[" .. ADDON_NAME .. "]"), ...)
     end
   end
 
+  --- Prints the given arguments with debug formatting.
+  --- @param ... any
   function Addon:Debug(...)
+    --@debug@
     print(date("%H:%M:%S"), Colors.Red("[Debug]"), ...)
+    --@end-debug@
   end
 end
