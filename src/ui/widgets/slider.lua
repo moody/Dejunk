8b8b02c1d229d5fd16ec651325796434f153ceb5
moody <moody@users.noreply.github.com>
2024-09-10 20:16:46 -0400

    update slider code

--- src/ui/widgets/slider.lua
+++ src/ui/widgets/slider.lua
@@ -10,7 +10,10 @@ local Widgets = Addon:GetModule("Widgets")
 
 --- @class SliderWidgetOptions : FrameWidgetOptions
 --- @field orientation? "VERTICAL" | "HORIZONTAL"
+--- @field minValue? number
+--- @field maxValue? number
 --- @field valueStep? number
+--- @field set? fun(value: number)
 
 -- =============================================================================
 -- Widgets - Slider
@@ -23,9 +26,11 @@ function Widgets:Slider(options)
   -- Defaults.
   options.name = Addon:IfNil(options.name, Widgets:GetUniqueName("Slider"))
   options.frameType = "Slider"
-  options.onUpdateTooltip = nil
   options.width = Addon:IfNil(options.width, 12)
+  options.height = Addon:IfNil(options.height, 12)
   options.orientation = Addon:IfNil(options.orientation, "VERTICAL")
+  options.minValue = Addon:IfNil(options.minValue, 0)
+  options.maxValue = Addon:IfNil(options.maxValue, 1)
   options.valueStep = Addon:IfNil(options.valueStep, 1)
 
   --- @class SliderWidget : FrameWidget, Slider
@@ -36,14 +41,31 @@ function Widgets:Slider(options)
   frame:SetObeyStepOnDrag(true)
   frame:SetOrientation(options.orientation)
   frame:SetValueStep(options.valueStep)
-  frame:SetMinMaxValues(0, 0)
-  frame:SetValue(0)
+  frame:SetMinMaxValues(options.minValue, options.maxValue)
+  frame:SetValue(options.minValue)
 
   -- Thumb texture.
   frame.texture = frame:CreateTexture("$parent_Texture", "ARTWORK")
   frame.texture:SetColorTexture(Colors.White:GetRGBA(0.25))
-  frame.texture:SetSize(frame:GetWidth(), frame:GetWidth() * 2)
   frame:SetThumbTexture(frame.texture)
+
+  -- Set texture size.
+  if options.orientation == "VERTICAL" then
+    local size = frame:GetWidth()
+    frame.texture:SetSize(size, size * 2)
+  else
+    local size = frame:GetHeight()
+    frame.texture:SetSize(size * 1.25, size)
+  end
+
+  frame:SetScript("OnMouseDown", function()
+    frame.texture:SetColorTexture(Colors.White:GetRGBA(0.5))
+  end)
+
+  frame:SetScript("OnMouseUp", function()
+    frame.texture:SetColorTexture(Colors.White:GetRGBA(0.25))
+    if options.set then options.set(frame:GetValue()) end
+  end)
 
   return frame
 end
