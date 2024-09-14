832a2dbb979fdc8d07cd8c6bc726d89146afb97a
moody <moody@users.noreply.github.com>
2024-09-13 23:23:56 -0400

    refactor MerchantButton again

--- src/ui/merchant-button.lua
+++ src/ui/merchant-button.lua
@@ -3,11 +3,10 @@ local Addon = select(2, ...) ---@type Addon
 local Actions = Addon:GetModule("Actions")
 local Colors = Addon:GetModule("Colors")
 local Commands = Addon:GetModule("Commands")
-local E = Addon:GetModule("Events")
-local EventManager = Addon:GetModule("EventManager")
 local JunkFilter = Addon:GetModule("JunkFilter")
 local L = Addon:GetModule("Locale")
 local StateManager = Addon:GetModule("StateManager")
+local TickerManager = Addon:GetModule("TickerManager")
 local Tooltip = Addon:GetModule("Tooltip")
 local Widgets = Addon:GetModule("Widgets")
 
@@ -15,70 +14,79 @@ local LABEL_TEXT_FORMAT = ("%s %s"):format(ADDON_NAME, Colors.Grey("(%s)"):forma
 
 local junkItems = {}
 
-EventManager:Once(E.Wow.MerchantShow, function()
-  --- @class MerchantButtonWidget : ButtonWidget
-  local frame = Widgets:Button({
-    parent = MerchantFrame,
-    name = ADDON_NAME .. "_MerchantButton",
-    width = 100,
-    labelText = LABEL_TEXT_FORMAT:format(0),
-    labelColor = Colors.Blue,
-    enableClickHandling = true
-  })
+-- ============================================================================
+-- Initialize
+-- ============================================================================
 
-  Widgets:ConfigureForDrag(frame)
-  Widgets:ConfigureForPointSync(frame, "MerchantButton")
+--- @class MerchantButtonWidget : ButtonWidget
+local frame = Widgets:Button({
+  name = ADDON_NAME .. "_MerchantButton",
+  width = 100,
+  labelText = LABEL_TEXT_FORMAT:format(0),
+  labelColor = Colors.Blue,
+  assignFrameLevel = true,
+  enableClickHandling = true
+})
 
-  -- Click handlers.
-  frame:SetClickHandler("LeftButton", "NONE", Commands.sell)
-  frame:SetClickHandler("LeftButton", "SHIFT", Commands.junk)
-  frame:SetClickHandler("RightButton", "NONE", Commands.options)
-  frame:SetClickHandler("RightButton", "SHIFT", function()
-    StateManager:Dispatch(Actions:ResetMerchantButtonPoint())
-  end)
+Widgets:ConfigureForDrag(frame)
+Widgets:ConfigureForPointSync(frame, "MerchantButton")
 
-  -- MerchantFrame OnUpdate.
-  MerchantFrame:HookScript("OnUpdate", function()
-    if StateManager:GetGlobalState().merchantButton then frame:Show() else frame:Hide() end
-  end)
+-- Click handlers.
+frame:SetClickHandler("LeftButton", "NONE", Commands.sell)
+frame:SetClickHandler("LeftButton", "SHIFT", Commands.junk)
+frame:SetClickHandler("RightButton", "NONE", Commands.options)
+frame:SetClickHandler("RightButton", "SHIFT", function()
+  StateManager:Dispatch(Actions:ResetMerchantButtonPoint())
+end)
 
-  -- OnUpdate.
-  frame:HookScript("OnUpdate", function(_, elapsed)
-    frame:SetEnabled(not Addon:IsBusy())
+-- OnUpdate.
+frame:HookScript("OnUpdate", function(_, elapsed)
+  frame:SetEnabled(not Addon:IsBusy())
 
-    frame.delayTimer = (frame.delayTimer or 0.1) + elapsed
-    if frame.delayTimer < 0.1 then return end
-    frame.delayTimer = 0
+  frame.delayTimer = (frame.delayTimer or 0.1) + elapsed
+  if frame.delayTimer < 0.1 then return end
+  frame.delayTimer = 0
 
-    JunkFilter:GetSellableJunkItems(junkItems)
-    frame.label:SetText(LABEL_TEXT_FORMAT:format(#junkItems))
-  end)
+  JunkFilter:GetSellableJunkItems(junkItems)
+  frame.label:SetText(LABEL_TEXT_FORMAT:format(#junkItems))
+end)
 
-  -- OnEnter.
-  frame:HookScript("OnEnter", function()
-    Tooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
-    Tooltip:AddDoubleLine(Colors.Blue(ADDON_NAME), Colors.Grey(Addon.VERSION))
-    Tooltip:AddLine(Addon:SubjectDescription(L.LEFT_CLICK, L.START_SELLING))
-    Tooltip:AddLine(Addon:SubjectDescription(L.RIGHT_CLICK, L.TOGGLE_OPTIONS_FRAME))
-    Tooltip:AddLine(Addon:SubjectDescription(Addon:Concat("+", L.SHIFT_KEY, L.LEFT_CLICK), L.TOGGLE_JUNK_FRAME))
-    Tooltip:AddLine(Addon:SubjectDescription(Addon:Concat("+", L.SHIFT_KEY, L.RIGHT_CLICK), L.RESET_POSITION))
-    Tooltip:Show()
-  end)
+-- OnEnter.
+frame:HookScript("OnEnter", function()
+  Tooltip:SetOwner(frame, "ANCHOR_RIGHT")
+  Tooltip:AddDoubleLine(Colors.Blue(ADDON_NAME), Colors.Grey(Addon.VERSION))
+  Tooltip:AddLine(Addon:SubjectDescription(L.LEFT_CLICK, L.START_SELLING))
+  Tooltip:AddLine(Addon:SubjectDescription(L.RIGHT_CLICK, L.TOGGLE_OPTIONS_FRAME))
+  Tooltip:AddLine(Addon:SubjectDescription(Addon:Concat("+", L.SHIFT_KEY, L.LEFT_CLICK), L.TOGGLE_JUNK_FRAME))
+  Tooltip:AddLine(Addon:SubjectDescription(Addon:Concat("+", L.SHIFT_KEY, L.RIGHT_CLICK), L.RESET_POSITION))
+  Tooltip:Show()
+end)
 
-  -- OnLeave.
-  frame:HookScript("OnLeave", function()
-    Tooltip:Hide()
-  end)
+-- OnLeave.
+frame:HookScript("OnLeave", function()
+  Tooltip:Hide()
+end)
 
-  -- OnDragStart.
-  frame:HookScript("OnDragStart", function()
-    frame:GetScript("OnLeave")(frame)
-  end)
+-- OnDragStart.
+frame:HookScript("OnDragStart", function()
+  frame:GetScript("OnLeave")(frame)
+end)
 
-  -- OnDragStop.
-  frame:HookScript("OnDragStop", function()
-    if frame:IsMouseOver() then
-      frame:GetScript("OnEnter")(frame)
-    end
-  end)
+-- OnDragStop.
+frame:HookScript("OnDragStop", function()
+  if frame:IsMouseOver() then
+    frame:GetScript("OnEnter")(frame)
+  end
+end)
+
+-- ============================================================================
+-- Ticker to update visibility.
+-- ============================================================================
+
+TickerManager:NewTicker(0.01, function()
+  if MerchantFrame and MerchantFrame:IsShown() and StateManager:GetGlobalState().merchantButton then
+    frame:Show()
+  else
+    frame:Hide()
+  end
 end)
