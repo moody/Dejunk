557d396fc594110b319f8bbf9e3710316724ce90
moody <moody@users.noreply.github.com>
2026-09-10 15:36:18 -0400

    refactor JunkFrame

--- src/ui/junk-frame.lua
+++ src/ui/junk-frame.lua
@@ -18,6 +18,254 @@ local Widgets = Addon:GetModule("Widgets")
 --- @class JunkFrame
 local JunkFrame = Addon:GetModule("JunkFrame")
 
+local Components = {}
+
+-- ============================================================================
+-- Local Functions
+-- ============================================================================
+
+local junkItems = {}
+
+local function hasSellableItems(items)
+  for _, item in ipairs(items) do
+    if Items:IsItemSellable(item) then
+      return true
+    end
+  end
+  return false
+end
+
+local function refreshComponents()
+  JunkFilter:GetJunkItems(junkItems)
+
+  Components.TitleText:GetFrame():SetText(
+    Colors.Yellow(("%s (%s)"):format(L.JUNK_ITEMS, Colors.White(#junkItems)))
+  )
+
+  local totalJunkValue = 0
+  for _, item in pairs(junkItems) do
+    totalJunkValue = totalJunkValue + (item.noValue and 0 or (item.price * item.quantity))
+  end
+  Components.ItemsFrame:GetFrame().title:SetText(Colors.White(GetCoinTextureString(totalJunkValue)))
+
+  if Addon:IsBusy() then
+    Components.StartSellingButton:GetFrame():SetEnabled(false)
+    Components.DestroyNextItemButton:GetFrame():SetEnabled(false)
+  else
+    Components.StartSellingButton:GetFrame():SetEnabled(Addon:IsAtMerchant() and hasSellableItems(junkItems))
+    Components.DestroyNextItemButton:GetFrame():SetEnabled(#junkItems > 0)
+  end
+end
+
+-- ============================================================================
+-- Root Component
+-- ============================================================================
+
+Components.Root = Addon.Waffle:Flex({
+  width = 325,
+  height = 375,
+  direction = "COLUMN",
+  hidden = true,
+
+  defaultFrameFactory = function(parent)
+    return CreateFrame("Frame")
+  end,
+
+  frameFactory = function(parent)
+    local frame = Widgets:Frame({
+      name = ADDON_NAME .. "_JunkFrame",
+      enableClickHandling = true,
+      enableDragging = true
+    })
+
+    frame:SetClickHandler("RightButton", "SHIFT", function()
+      StateManager:Dispatch(ActionCreators.Global.points.junkFrame.reset())
+    end)
+
+    Widgets:ConfigureForPointSync(frame, "JunkFrame")
+
+    table.insert(UISpecialFrames, frame:GetName())
+
+    local delayTimer = 0
+    frame:SetScript("OnUpdate", function(_, elapsed)
+      delayTimer = delayTimer + elapsed
+      if delayTimer < 0.02 then return end
+      delayTimer = 0
+      refreshComponents()
+    end)
+
+    frame:Hide()
+    return frame
+  end
+})
+
+-- ============================================================================
+-- Title Components
+-- ============================================================================
+
+Components.TitleRow = Components.Root:AddRow({
+  height = 32,
+  paddingLeft = Widgets:Padding(),
+  frameFactory = function(parent)
+    local frame = Widgets:Frame({ parent = parent })
+    frame:SetBackdropColor(Colors.DarkGrey:GetRGB())
+    return frame
+  end
+})
+
+Components.TitleText = Components.TitleRow:AddChild({
+  --- @param parent Frame
+  frameFactory = function(parent)
+    local fontString = parent:CreateFontString("$parent_TitleText", "ARTWORK", "GameFontNormalLarge")
+    fontString:SetJustifyH("LEFT")
+    fontString:SetText(Colors.Yellow(L.JUNK_ITEMS))
+    return fontString
+  end
+})
+
+Components.CloseButton = Components.TitleRow:AddChild({
+  width = 46,
+  frameFactory = function(parent)
+    return Widgets:TitleFrameIconButton({
+      name = "$parent_CloseButton",
+      texture = Addon:GetAsset("x-icon"),
+      textureSize = 14,
+      highlightColor = Colors.Red,
+      onClick = function() JunkFrame:Hide() end
+    })
+  end
+})
+
+-- ============================================================================
+-- Content Components
+-- ============================================================================
+
+Components.Content = Components.Root:AddChild({
+  direction = "COLUMN",
+  padding = Widgets:Padding(),
+  gap = Widgets:Padding(0.5),
+})
+
+Components.ItemsFrame = Components.Content:AddChild({
+  frameFactory = function()
+    local itemsFrame = Widgets:ItemsFrame({
+      name = "$parent_ItemsFrame",
+      displayPrice = true,
+      titleText = Colors.White(L.JUNK_ITEMS),
+      onUpdateTooltip = function(self, tooltip)
+        tooltip:SetText(L.JUNK_ITEMS)
+        tooltip:AddLine(L.JUNK_FRAME_TOOLTIP:format(
+          Lists.ProfileInclusions.name,
+          Lists.GlobalInclusions.name,
+          Colors.White(L.SHIFT_KEY)
+        ))
+        tooltip:AddLine(" ")
+        tooltip:AddDoubleLine(
+          Addon:Concat("+", L.CONTROL_KEY, L.ALT_KEY, L.RIGHT_CLICK),
+          L.ADD_ALL_TO_LIST:format(Lists.ProfileExclusions.name)
+        )
+        tooltip:AddDoubleLine(
+          Addon:Concat("+", L.CONTROL_KEY, L.ALT_KEY, L.SHIFT_KEY, L.RIGHT_CLICK),
+          L.ADD_ALL_TO_LIST:format(Lists.GlobalExclusions.name)
+        )
+      end,
+      itemButtonOnUpdateTooltip = function(self, tooltip)
+        tooltip:SetOwner(self, "ANCHOR_RIGHT")
+        tooltip:SetBagItem(self.item.bag, self.item.slot)
+        tooltip:AddLine(" ")
+        tooltip:AddDoubleLine(L.LEFT_CLICK, L.SELL)
+        tooltip:AddDoubleLine(L.RIGHT_CLICK, L.ADD_TO_LIST:format(Lists.ProfileExclusions.name))
+        tooltip:AddDoubleLine(
+          Addon:Concat("+", L.SHIFT_KEY, L.RIGHT_CLICK),
+          L.ADD_TO_LIST:format(Lists.GlobalExclusions.name)
+        )
+        tooltip:AddDoubleLine(Addon:Concat("+", L.ALT_KEY, L.RIGHT_CLICK), Colors.Red(L.DESTROY))
+      end,
+      itemButtonOnClick = function(self, button)
+        if button == "LeftButton" then
+          Seller:HandleItem(self.item)
+        end
+
+        if button == "RightButton" then
+          if IsAltKeyDown() then
+            Destroyer:HandleItem(self.item)
+          else
+            local list = IsShiftKeyDown() and Lists.GlobalExclusions or Lists.ProfileExclusions
+            list:Add(self.item.id)
+          end
+        end
+      end,
+      getItems = function() return junkItems end,
+      addItem = function(itemId)
+        local list = IsShiftKeyDown() and Lists.GlobalInclusions or Lists.ProfileInclusions
+        list:Add(itemId)
+      end,
+      removeAllItems = function()
+        local list = IsShiftKeyDown() and Lists.GlobalExclusions or Lists.ProfileExclusions
+        for _, item in pairs(junkItems) do list:Add(item.id) end
+      end
+    })
+
+    return itemsFrame
+  end
+})
+
+Components.ButtonRow = Components.Content:AddRow({
+  maxHeight = 30,
+  gap = Widgets:Padding(0.5)
+})
+
+Components.StartSellingButton = Components.ButtonRow:AddChild({
+  frameFactory = function(parent)
+    return Widgets:Button({
+      name = "$parent_StartSellingButton",
+      labelColor = Colors.Yellow,
+      labelText = L.START_SELLING,
+      onClick = Commands.sell
+    })
+  end
+})
+
+Components.DestroyNextItemButton = Components.ButtonRow:AddChild({
+  frameFactory = function(parent)
+    return Widgets:Button({
+      name = "$parent_DestroyNextItemButton",
+      labelColor = Colors.Red,
+      labelText = L.DESTROY_NEXT_ITEM,
+      onClick = Commands.destroy,
+      onUpdateTooltip = function(self, tooltip)
+        tooltip:SetOwner(self, "ANCHOR_RIGHT")
+        local item = JunkFilter:GetNextDestroyableJunkItem()
+        if item then
+          tooltip:SetBagItem(item.bag, item.slot)
+        end
+      end
+    })
+  end
+})
+
+-- ============================================================================
+-- JunkFrame
+-- ============================================================================
+
+function JunkFrame:Show()
+  Components.Root:SetHidden(false)
+  Components.Root:Layout()
+end
+
+function JunkFrame:Hide()
+  Components.Root:SetHidden(true)
+  Components.Root:Layout()
+end
+
+function JunkFrame:Toggle()
+  if Components.Root:GetHidden() then
+    self:Show()
+  else
+    self:Hide()
+  end
+end
+
 -- ============================================================================
 -- Events
 -- ============================================================================
@@ -31,184 +279,3 @@ EventManager:Once(E.StoreCreated, function()
     if StateManager:GetProfileState().settings.autoJunkFrame then JunkFrame:Hide() end
   end)
 end)
-
--- ============================================================================
--- JunkFrame
--- ============================================================================
-
-function JunkFrame:Show()
-  self.frame:Show()
-end
-
-function JunkFrame:Hide()
-  self.frame:Hide()
-end
-
-function JunkFrame:Toggle()
-  if self.frame:IsShown() then
-    self.frame:Hide()
-  else
-    self.frame:Show()
-  end
-end
-
--- ============================================================================
--- Initialize
--- ============================================================================
-
-local function hasSellableItems(items)
-  for _, item in ipairs(items) do
-    if Items:IsItemSellable(item) then
-      return true
-    end
-  end
-  return false
-end
-
--- Create frame.
-JunkFrame.frame = (function()
-  --- @class JunkFrameWidget : WindowWidget
-  local frame = Widgets:Window({
-    name = ADDON_NAME .. "_JunkFrame",
-    width = 325,
-    height = 375,
-    titleText = Colors.Yellow(L.JUNK_ITEMS),
-    enableClickHandling = true
-  })
-  frame.items = {}
-
-  frame:SetClickHandler("RightButton", "SHIFT", function()
-    StateManager:Dispatch(ActionCreators.Global.points.junkFrame.reset())
-  end)
-
-  Widgets:ConfigureForPointSync(frame, "JunkFrame")
-
-  -- Start selling button.
-  frame.startSellingButton = Widgets:Button({
-    name = "$parent_StartSellingButton",
-    parent = frame,
-    points = {
-      { "BOTTOMLEFT", frame, Widgets:Padding(), Widgets:Padding() },
-      { "BOTTOMRIGHT", frame, "BOTTOM", -Widgets:Padding(0.25), Widgets:Padding() }
-    },
-    labelColor = Colors.Yellow,
-    labelText = L.START_SELLING,
-    onClick = Commands.sell
-  })
-
-  -- Destroy next item button.
-  frame.destroyNextItemButton = Widgets:Button({
-    name = "$parent_DestroyNextItemButton",
-    parent = frame,
-    points = {
-      { "BOTTOMLEFT", frame, "BOTTOM", Widgets:Padding(0.25), Widgets:Padding() },
-      { "BOTTOMRIGHT", frame, -Widgets:Padding(), Widgets:Padding() }
-    },
-    labelColor = Colors.Red,
-    labelText = L.DESTROY_NEXT_ITEM,
-    onClick = Commands.destroy,
-    onUpdateTooltip = function(self, tooltip)
-      tooltip:SetOwner(self, "ANCHOR_RIGHT")
-      local item = JunkFilter:GetNextDestroyableJunkItem()
-      if item then
-        tooltip:SetBagItem(item.bag, item.slot)
-      end
-    end
-  })
-
-  frame:HookScript("OnUpdate", function(self, elapsed)
-    self.delayTimer = (self.delayTimer or 0) + elapsed
-    if self.delayTimer < 0.02 then return end
-    self.delayTimer = 0
-
-    -- Get items.
-    JunkFilter:GetJunkItems(self.items)
-
-    -- Title.
-    self.title:SetText(Colors.Yellow(("%s (%s)"):format(L.JUNK_ITEMS, Colors.White(#self.items))))
-
-    -- Buttons.
-    if Addon:IsBusy() then
-      self.startSellingButton:SetEnabled(false)
-      self.destroyNextItemButton:SetEnabled(false)
-    else
-      self.startSellingButton:SetEnabled(Addon:IsAtMerchant() and hasSellableItems(self.items))
-      self.destroyNextItemButton:SetEnabled(#self.items > 0)
-    end
-  end)
-
-  -- Items frame.
-  frame.itemsFrame = Widgets:ItemsFrame({
-    name = "$parent_ItemsFrame",
-    parent = frame,
-    points = {
-      { "TOPLEFT", frame.titleButton, "BOTTOMLEFT", Widgets:Padding(), 0 },
-      { "BOTTOMRIGHT", frame.destroyNextItemButton, "TOPRIGHT", 0, Widgets:Padding(0.5) }
-    },
-    displayPrice = true,
-    titleText = Colors.White(L.JUNK_ITEMS),
-    onUpdateTooltip = function(self, tooltip)
-      tooltip:SetText(L.JUNK_ITEMS)
-      tooltip:AddLine(L.JUNK_FRAME_TOOLTIP:format(
-        Lists.ProfileInclusions.name,
-        Lists.GlobalInclusions.name,
-        Colors.White(L.SHIFT_KEY)
-      ))
-      tooltip:AddLine(" ")
-      tooltip:AddDoubleLine(
-        Addon:Concat("+", L.CONTROL_KEY, L.ALT_KEY, L.RIGHT_CLICK),
-        L.ADD_ALL_TO_LIST:format(Lists.ProfileExclusions.name)
-      )
-      tooltip:AddDoubleLine(
-        Addon:Concat("+", L.CONTROL_KEY, L.ALT_KEY, L.SHIFT_KEY, L.RIGHT_CLICK),
-        L.ADD_ALL_TO_LIST:format(Lists.GlobalExclusions.name)
-      )
-    end,
-    itemButtonOnUpdateTooltip = function(self, tooltip)
-      tooltip:SetOwner(self, "ANCHOR_RIGHT")
-      tooltip:SetBagItem(self.item.bag, self.item.slot)
-      tooltip:AddLine(" ")
-      tooltip:AddDoubleLine(L.LEFT_CLICK, L.SELL)
-      tooltip:AddDoubleLine(L.RIGHT_CLICK, L.ADD_TO_LIST:format(Lists.ProfileExclusions.name))
-      tooltip:AddDoubleLine(
-        Addon:Concat("+", L.SHIFT_KEY, L.RIGHT_CLICK),
-        L.ADD_TO_LIST:format(Lists.GlobalExclusions.name)
-      )
-      tooltip:AddDoubleLine(Addon:Concat("+", L.ALT_KEY, L.RIGHT_CLICK), Colors.Red(L.DESTROY))
-    end,
-    itemButtonOnClick = function(self, button)
-      if button == "LeftButton" then
-        Seller:HandleItem(self.item)
-      end
-
-      if button == "RightButton" then
-        if IsAltKeyDown() then
-          Destroyer:HandleItem(self.item)
-        else
-          local list = IsShiftKeyDown() and Lists.GlobalExclusions or Lists.ProfileExclusions
-          list:Add(self.item.id)
-        end
-      end
-    end,
-    getItems = function() return frame.items end,
-    addItem = function(itemId)
-      local list = IsShiftKeyDown() and Lists.GlobalInclusions or Lists.ProfileInclusions
-      list:Add(itemId)
-    end,
-    removeAllItems = function()
-      local list = IsShiftKeyDown() and Lists.GlobalExclusions or Lists.ProfileExclusions
-      for _, item in pairs(frame.items) do list:Add(item.id) end
-    end
-  })
-
-  frame.itemsFrame:HookScript("OnUpdate", function(self)
-    local totalJunkValue = 0
-    for _, item in pairs(frame.items) do
-      local itemValue = item.noValue and 0 or (item.price * item.quantity)
-      totalJunkValue = totalJunkValue + itemValue
-    end
-    self.title:SetText(Colors.White(GetCoinTextureString(totalJunkValue)))
-  end)
-
-  return frame
-end)()
