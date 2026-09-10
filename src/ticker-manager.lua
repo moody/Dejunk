84dd4bc93724c246617f616189d9b15b29305f51
moody <moody@users.noreply.github.com>
2026-09-09 22:44:09 -0400

    refactor TickerManager

--- src/ticker-manager.lua
+++ src/ticker-manager.lua
@@ -3,34 +3,30 @@ local Addon = select(2, ...) ---@type Addon
 --- @class TickerManager
 local TickerManager = Addon:GetModule("TickerManager")
 
---- @class Ticker
---- @field protected callback function
---- @field protected isActive boolean
---- @field protected maxTicks number
---- @field protected ticks number
---- @field protected timePerTick number
---- @field protected timer number
-
 --- @type table<Ticker, boolean>
 local activeTickers = {}
 
--- ============================================================================
 -- Frame for updating active tickers.
--- ============================================================================
-
 CreateFrame("Frame"):SetScript("OnUpdate", function(_, elapsed)
   for ticker in pairs(activeTickers) do ticker:OnUpdate(elapsed) end
 end)
 
 -- ============================================================================
--- Mixins
+-- Ticker
 -- ============================================================================
 
 --- @class Ticker
-local TickerMixins = {}
+--- @field package callback function
+--- @field package isActive boolean
+--- @field package maxTicks number
+--- @field package ticks number
+--- @field package timePerTick number
+--- @field package timer number
+local Ticker = {}
+Ticker.__index = Ticker
 
 --- Reactivates the ticker and resets its timer and tick count.
-function TickerMixins:Restart()
+function Ticker:Restart()
   self.timer = 0
   self.ticks = 0
   self.isActive = true
@@ -38,20 +34,20 @@ function TickerMixins:Restart()
 end
 
 --- Deactivates the ticker.
-function TickerMixins:Cancel()
+function Ticker:Cancel()
   self.isActive = false
   activeTickers[self] = nil
 end
 
 --- Returns `true` if the ticker is not active.
 --- @return boolean
-function TickerMixins:IsCancelled()
+function Ticker:IsCancelled()
   return not self.isActive
 end
 
 --- Updates the ticker's timer and executes its callback as necessary.
 --- @param elapsed number The time since the last update
-function TickerMixins:OnUpdate(elapsed)
+function Ticker:OnUpdate(elapsed)
   if not self.isActive then return end
   if self.maxTicks > 0 and self.ticks >= self.maxTicks then
     return self:Cancel()
@@ -66,7 +62,7 @@ function TickerMixins:OnUpdate(elapsed)
 end
 
 -- ============================================================================
--- Functions
+-- TickerManager
 -- ============================================================================
 
 --- Creates a new ticker which executes a callback at a specified interval,
@@ -76,17 +72,13 @@ end
 --- @param iterations? number If `0` (default), the ticker will be called indefinitely.
 --- @return Ticker ticker
 function TickerManager:NewTicker(seconds, callback, iterations)
-  --- @class (exact) Ticker
-  local ticker = {
+  local ticker = setmetatable({
     callback = callback,
     maxTicks = iterations or 0,
     ticks = 0,
     timePerTick = seconds,
     timer = 0
-  }
-
-  -- Mixins.
-  for k, v in pairs(TickerMixins) do ticker[k] = v end
+  }, Ticker)
 
   -- Set active.
   ticker.isActive = true
